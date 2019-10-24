f08ebde90e90429550f0534fb85a3a9e4c5ad4c5
Kenji Azaki <kenji_azaki@kenjiganbarenai.local>
2019-10-24 23:32:16 +0900

    first commit

---
+++ app/src/main/java/com/example/change_textviewstep1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.change_textviewstep1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
